0519c57ae919f6c2c5c1a5c91a4768475df27ea3
IngaPirogova <ingeborga.pirogova@gmail.com>
2023-03-17 16:13:08 +0200

    summary

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,15 +1,20 @@
 import { useAuth } from "hooks";
 import { HomeAuthNav } from "components/HomeAuthNav/HomeAuthNav";
 
+
 export const HomePage = () => {
     const { isLoggedIn } = useAuth();
   
     return (
       <>
+      
         <section >
+           
           <h1 >Welcome to Phonebook!</h1>          
           {!isLoggedIn && <HomeAuthNav />}
+
         </section>
+      
         
       </>
     );
