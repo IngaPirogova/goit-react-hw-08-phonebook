38dc1315475528a47204d2060628d0e45532deca
IngaPirogova <ingeborga.pirogova@gmail.com>
2023-03-17 16:22:41 +0200

    Sum

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,21 +1,17 @@
-import { useAuth } from "hooks";
-import { HomeAuthNav } from "components/HomeAuthNav/HomeAuthNav";
-
+import { useAuth } from 'hooks';
+import { HomeAuthNav } from 'components/HomeAuthNav/HomeAuthNav';
+import css from './HomePage.module.css';
 
 export const HomePage = () => {
-    const { isLoggedIn } = useAuth();
-  
-    return (
-      <>
-      
-        <section >
-           
-          <h1 >Welcome to Phonebook!</h1>          
-          {!isLoggedIn && <HomeAuthNav />}
+  const { isLoggedIn } = useAuth();
 
-        </section>
-      
-        
-      </>
-    );
-  };
+  return (
+    <>
+      <section>
+        <h1>Welcome to the phonebook</h1>
+        <p className={css.text}>please,</p>
+        {!isLoggedIn && <HomeAuthNav />}
+      </section>
+    </>
+  );
+};
